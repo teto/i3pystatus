61dfb60e46d8246ac69dbfff49744b42e100dc04
Matthieu Coudron <mattator@gmail.com>
2014-12-18 00:09:25 +0100

    This is a hack to work around notmuch's API limitations. We redirect stderr and if the library outputted sthg, we reopen the notmuch database. More infos on https://github.com/enkore/i3pystatus/issues/121 The fix is only valid for python3.4

--- i3pystatus/mail/notmuchmail.py
+++ i3pystatus/mail/notmuchmail.py
@@ -6,7 +6,12 @@
 import notmuch
 import configparser
 import os
+import io
+import sys
+from contextlib import redirect_stdout
+
 from i3pystatus.mail import Backend
+from i3pystatus import logger
 
 
 class Notmuch(Backend):
@@ -21,6 +26,7 @@ class Notmuch(Backend):
     )
 
     db_path = None
+    db = None
 
     def init(self):
         if not self.db_path:
@@ -28,18 +34,34 @@ class Notmuch(Backend):
             config = configparser.RawConfigParser()
 
             # read tries to read and returns successfully read filenames
-            successful = config.read([
+            config.read([
                 os.environ.get("NOTMUCH_CONFIG", defaultConfigFilename),
                 defaultConfigFilename
             ])
 
             self.db_path = config.get("database", "path")
 
+    def _open_db(self):
         self.db = notmuch.Database(self.db_path)
+        return self.db
 
     @property
     def unread(self):
-        return notmuch.Query(self.db, "tag:unread and tag:inbox").count_messages()
+        self.db = self.db if self.db else self._open_db()
+
+        # only valid for python3.4
+        # https://docs.python.org/3.4/library/contextlib.html#contextlib.redirect_stdout
+        if sys.hexversion > 0x03040000:
+            f = io.StringIO()
+            with redirect_stdout(f):
+                res = notmuch.Query(self.db, "tag:unread and tag:inbox").count_messages()
+                if f.getvalue():
+                    logger.warning("Notmuch error" + f.getvalue())
+                    self.db = None
+        else:
+            res = notmuch.Query(self.db, "tag:unread and tag:inbox").count_messages()
+
+        return res
 
 
 Backend = Notmuch
